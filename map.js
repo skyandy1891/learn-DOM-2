512e38eb839a853e546e147a28b7b028920eadc6
skyandy1891 <skyandy1891@gmail.com>
2021-07-25 12:59:56 +0300

    вспомнил 'map'

---
+++ map.js
@@ -0,0 +1,40 @@
+// const users = [
+//   { name: "Mango", isActive: true },
+//   { name: "Poly", isActive: false },
+//   { name: "Ajax", isActive: true },
+// ];
+
+// // Для каждого элемента коллекции (user) вернем значение поля name
+// const names = users.map((user) => user.isActive);
+// console.log(names); // ["Mango", "Poly", "Ajax"]
+
+const books = [
+  { title: "Последнее королевство", author: "Бернард Корнуэлл", rating: 8.38 },
+  { title: "На берегу спокойных вод", author: "Роберт Шекли", rating: 8.51 },
+  { title: "Сон смешного человека", author: "Федор Достоевский", rating: 7.75 },
+  { title: "Красна как кровь", author: "Ли Танит", rating: 7.94 },
+  { title: "Враг Божий", author: "Бернард Корнуэлл", rating: 8.67 },
+];
+
+const titles = books.map((book) => book.title);
+
+const autors = books.map((book) => book.author);
+
+// ===================task 13 module 6=============
+
+// Дополни функцию getUserNames(users) так, чтобы она возвращала массив
+// имён пользователей(свойство name) из массива объектов в параметре users.
+
+// const getUserNames = (users) => {
+//   const names = users.map((user) => user.name);
+//   return names;
+// };
+// ====================task 14 module 6=============================
+
+// Дополни функцию getUserEmails(users) так, чтобы она возвращала массив почтовых
+// адресов пользователей(свойство email) из массива объектов в параметре users.
+
+// const getUserEmails = (users) => {
+//   const email = users.map((user) => user.email);
+//   return email;
+// };
